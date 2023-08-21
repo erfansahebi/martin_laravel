a01be0178f290ef7b2bff58b6cf4e464eda386e1
Erfan Sahebi <erfansahebivayghan@gmail.com>
2023-08-22 00:24:42 +0330

    Fix(base-structure): fix users factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,37 +2,46 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition (): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'name'         => fake()->name(),
+            'phone_number' => $this->generatePhoneNumber(),
+            'password'     => \Hash::make( value: '123456789' ),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function configure (): Factory|UserFactory
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->afterCreating( function ( User $user ) {
+
+        } );
     }
+
+    private function generatePhoneNumber (): string
+    {
+        $phone_number = '09';
+        for ( $i = 0; $i <= 9; $i ++ )
+        {
+            $phone_number .= rand( min: 0, max: 9 );
+        }
+
+        return normalize_phone_number( phone_number: $phone_number );
+    }
+
 }
