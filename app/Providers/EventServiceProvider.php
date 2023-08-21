691ab9878036a90cfa98cdad546889a86eec10ba
Erfan Sahebi <erfansahebivayghan@gmail.com>
2023-08-22 00:30:59 +0330

    Feat(base-structure): Assign observers to models

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Models\CourierLocation;
+use App\Models\Order;
+use App\Observers\CourierLocationObserver;
+use App\Observers\OrderObserver;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,23 +16,22 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
+
     ];
 
     /**
      * Register any events for your application.
      */
-    public function boot(): void
+    public function boot (): void
     {
-        //
+        Order::observe( OrderObserver::class );
+        CourierLocation::observe( CourierLocationObserver::class );
     }
 
     /**
      * Determine if events and listeners should be automatically discovered.
      */
-    public function shouldDiscoverEvents(): bool
+    public function shouldDiscoverEvents (): bool
     {
         return false;
     }
