279394223132ec6961ba468976367533d3e75025
Erfan Sahebi <erfansahebivayghan@gmail.com>
2023-08-22 00:29:51 +0330

    Feat(base-structure): Ignore passport routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,21 +3,22 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
-    public function register(): void
+    public function register (): void
     {
-        //
+        Passport::ignoreRoutes();
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot (): void
     {
         //
     }
